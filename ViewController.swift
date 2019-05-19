433fddf97d131fa268f8c335cbd6ad8d628333d4
Greg Tiu <gregtiu@gregs-mbp.home>
2019-05-19 00:40:08 -0400

    Create Repo

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pictavate2.0
+//
+//  Created by Greg Tiu on 5/19/19.
+//  Copyright © 2019 Greg Tiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
