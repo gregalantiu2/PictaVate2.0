edea6f35c68e98ff9a02062b30bd541b9e257620
Greg Tiu <gregtiu@gregs-mbp.home>
2019-05-27 13:47:09 -0400

    Added Motivational Quotes Functionality

--- PhotoViewController.swift
+++ PhotoViewController.swift
@@ -8,20 +8,46 @@
 
 import UIKit
 
+struct Quote{
+    var quote: String!
+    var author: String!
+}
+
 class PhotoViewController: UIViewController {
 
+    var Quotes = [Quote]()
+    
+    var QuoteID = Int()
 
     @IBOutlet weak var picta: UIImageView!
     
+    @IBOutlet weak var displayQuote: UILabel!
+    
+    @IBOutlet weak var displayAuthor: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //Get Uploaded Photo from UserDefaults
         if let imageData = UserDefaults.standard.data(forKey: "myImageKey"){
             self.picta.image = UIImage(data: imageData)
         }
+        
+        //Fill Motivational Quotes
+        Quotes = [Quote(quote: "Don't be afraid to give up the good to go for the great.", author: "John D. Rockefeller"),Quote(quote: "Work hard in silence, let your success be your noise.", author: "Frank Ocean")]
+        
+        PickQuote()
     }
     
-    
+    func PickQuote(){
+        
+        QuoteID = Int.random(in: 0..<Quotes.count)
+        
+        displayQuote.text = Quotes[QuoteID].quote
+        
+        displayAuthor.text = "-" + Quotes[QuoteID].author
+        
+    }
 
 }
 
